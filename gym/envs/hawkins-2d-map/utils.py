7e522c770676b71f2cda068f618df40d32815978
Adi <rauniyar@cmu.edu>
2023-04-05 20:45:44 -0400

    FIX: reorganizing the utils.py

--- gym/envs/hawkins-2d-map/utils.py
+++ gym/envs/hawkins-2d-map/utils.py
@@ -1,6 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+from enum import Enum
+
+
+class Color(Enum):
+    NO_COLOR = 0
+    GREEN = 1
+    WHITE = 2
+    ORANGE = 3
+    YELLOW = 4
+
+
+class LookScore(Enum):
+    NOT_GOOD = 0
+    GOOD = 1
+    WOW = 3
 
 
 def getObstacleMapFromPointcloud(pointcloud_filename, grid_size):
