728408a190c2bba178c760af663103b03d7e1a19
Adi <mail.adityarauniyar@gmail.com>
2023-04-12 19:44:31 -0400

    Test: Adding test world scenarios 2

--- tests/spaces/test_spaces.py
+++ tests/spaces/test_spaces.py
@@ -1,7 +1,8 @@
 import unittest
 import numpy as np
-from typing import Tuple, List
 from gym.spaces.space import Space
+from gym.spaces.drone_space import DroneSpace
+from gym.utils.types import List
 
 MAP_HEIGHT = 3
 MAP_WIDTH = 3
@@ -64,6 +65,54 @@ class TestSpace(unittest.TestCase):
 
         self.assertFalse(self.space.move_to((0, 0, 1)))
         self.assertEqual(self.space.current_position, (1, 0, 0))
+
+
+class TestDroneSpace(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.grid_size = 1.0
+        self.operational_map = np.ones((MAP_HEIGHT, MAP_WIDTH))
+        self.start_position = (0, 0, 0)
+        self.viewing_angle = 90.0
+        self.viewing_range = 15.0
+        self.observation_space_size = 10
+        self.agent_id = 0
+        self.env = DroneSpace(self.grid_size, self.operational_map, self.start_position, self.viewing_angle,
+                              self.viewing_range, self.observation_space_size, self.agent_id)
+
+    def test_initialization(self):
+        self.assertIsInstance(self.env, Space)
+        self.assertEqual(self.env.grid_size, self.grid_size)
+        np.testing.assert_array_equal(self.env.operational_map, self.operational_map)
+        self.assertEqual(self.env.start_position, self.start_position)
+        self.assertEqual(self.env.viewing_angle, self.viewing_angle)
+        self.assertEqual(self.env.viewing_range, self.viewing_range)
+        self.assertEqual(self.env.observation_space_size, self.observation_space_size)
+        self.assertEqual(self.env.agent_id, self.agent_id)
+
+    def test_get_camera_coverage_definition(self):
+        x, y, radius, arc_angle_in_deg, centerline_angle_deg = self.env.get_camera_coverage_definition()
+        self.assertIsInstance(x, int)
+        self.assertIsInstance(y, int)
+        self.assertIsInstance(radius, float)
+        self.assertIsInstance(arc_angle_in_deg, float)
+        self.assertIsInstance(centerline_angle_deg, int)
+
+    def test_current_cam_coverage_locations(self):
+        self.seed = seeding.create_seed(0)
+        self.np_random = np.random.RandomState(self.seed)
+        coverage = self.env.current_cam_coverage_locations
+        self.assertIsInstance(coverage, List)
+
+    def test_get_current_observation_channels(self):
+        self.seed = seeding.create_seed(0)
+        self.np_random = np.random.RandomState(self.seed)
+        current_agents_position_arr = [(0, 0), (2, 2), (3, 4)]
+        current_actor_position_arr = [(1, 1)]
+        observation_channels = self.env.get_current_observation_channels(current_agents_position_arr,
+                                                                         current_actor_position_arr)
+        self.assertIsInstance(observation_channels, np.ndarray)
+        self.assertEqual(observation_channels.shape, (self.observation_space_size, self.observation_space_size))
 
 
 if __name__ == '__main__':
